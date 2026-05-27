015e72288bea59d88bbbff722ef36e5a64077053
nelson idahosa <145884395+noikur@users.noreply.github.com>
2026-05-27 01:48:00 +0100

    Add files via upload

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace VoicePApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
